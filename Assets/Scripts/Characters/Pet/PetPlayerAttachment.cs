288eba5d4a41fe80fd7f60d24e4b84096e3a7c06
Raphael <rpanduro.t@gmail.com>
2024-04-07 02:15:30 -0500

    FIX Deleted unnecessary code

--- Assets/Scripts/Characters/Pet/PetPlayerAttachment.cs
+++ Assets/Scripts/Characters/Pet/PetPlayerAttachment.cs
@@ -7,10 +7,5 @@ public class PetPlayerAttachment : MonoBehaviour
     [Header("Settings")]
     [SerializeField] private bool attachToPlayer;
 
-    public bool AttachToPlayer { get; private set;}
-
-    private void Update()
-    {
-        AttachToPlayer = attachToPlayer;
-    }
+    public bool AttachToPlayer => attachToPlayer;
 }
