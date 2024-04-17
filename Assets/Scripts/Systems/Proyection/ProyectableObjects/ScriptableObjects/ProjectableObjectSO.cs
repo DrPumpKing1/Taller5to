86eaa42e7bc952b6b054bc6b8f8e5917ea90ad6e
Raphael <rpanduro.t@gmail.com>
2024-04-17 12:13:25 -0500

    17/04 Raphael Panduro
    ADD MagicBox Scriptable Object Ready to support MagicBox prefab

--- Assets/Scripts/Systems/Proyection/ProyectableObjects/ScriptableObjects/ProjectableObjectSO.cs
+++ Assets/Scripts/Systems/Proyection/ProyectableObjects/ScriptableObjects/ProjectableObjectSO.cs
@@ -8,6 +8,8 @@ public class ProjectableObjectSO : ScriptableObject
     [Header("Scriptable Object Settings")]
     public Transform prefab;
     public int projectionGemsCost;
-    public Sprite sprite;
     public string objectName;
+    public string description;
+    public Sprite sprite;
+    public Transform visualPrefab;
 }
