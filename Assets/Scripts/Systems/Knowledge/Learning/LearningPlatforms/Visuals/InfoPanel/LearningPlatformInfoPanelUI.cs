94e9d6e7d7a1fd8b8908b7d09e478bee019df37b
Raphael <rpanduro.t@gmail.com>
2024-04-18 02:04:00 -0500

    18/04 Raphael Panduro
    ADD LearningPlatform Info Panel Basic Functionality

--- Assets/Scripts/Systems/Knowledge/Learning/LearningPlatforms/Visuals/InfoPanel/LearningPlatformInfoPanelUI.cs
+++ Assets/Scripts/Systems/Knowledge/Learning/LearningPlatforms/Visuals/InfoPanel/LearningPlatformInfoPanelUI.cs
@@ -1,25 +1,59 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System;
+using TMPro;
+using UnityEngine.UI;
 
 public class LearningPlatformInfoPanelUI : MonoBehaviour
 {
-    [Header("Settings")]
-    [SerializeField] private float lifeTime;
+    [Header("Projectable Object UI Components")]
+    [SerializeField] private TextMeshProUGUI projectableObjectName;
+    [SerializeField] private Image projectableObjectImage;
+    [SerializeField] private TextMeshProUGUI projectableObjectDescription;
 
-    public float LifeTime { get { return lifeTime; } }
+    [Header("Dialect Requirements UI Components")]
+    [SerializeField] private Transform dialectKnowledgeRequirementsUIsContainer;
+    [SerializeField] private Transform dialectKnowledgeRequirementsTemplate;
 
-    private void Awake()
+    private void Start()
     {
-        //DestroyAfterLifetime();
+        DisableDialectRequirementsUITemplate();
     }
-
-    private void DestroyAfterLifetime() => Destroy(gameObject, lifeTime);
 
     public void SetInfoPanelContents(ProjectableObjectSO projectableObjectToLearn, List<DialectKnowledge> dialectKnowledgeRequirements)
     {
-
+        SetProjectableObjectContents(projectableObjectToLearn);
+        SetDialectKnowledgeRequirementsContents(dialectKnowledgeRequirements);
     }
+
+    private void SetProjectableObjectContents(ProjectableObjectSO projectableObjectToLearn)
+    {
+        projectableObjectName.text = projectableObjectToLearn.objectName;
+        projectableObjectImage.sprite = projectableObjectToLearn.sprite;
+        projectableObjectDescription.text = projectableObjectToLearn.description;
+    }
+
+    private void SetDialectKnowledgeRequirementsContents(List<DialectKnowledge> dialectKnowledgeRequirements)
+    {
+        foreach (DialectKnowledge dialectKnowledge in dialectKnowledgeRequirements)
+        {
+            Transform dialectRequirementsUIGameObject = Instantiate(dialectKnowledgeRequirementsTemplate, dialectKnowledgeRequirementsUIsContainer);
+            dialectRequirementsUIGameObject.gameObject.SetActive(true);
+
+            LearningPlatformKnowledgeRequirementsSingleUI knowledgeRequirementUI = dialectRequirementsUIGameObject.GetComponent<LearningPlatformKnowledgeRequirementsSingleUI>();
+
+            if (!knowledgeRequirementUI)
+            {
+                Debug.LogWarning("There's not a LearningPlatformKnowledgeRequirementsSingleUI attached to instantiated prefab");
+                continue;
+            }
+
+            knowledgeRequirementUI.SetDialectKnowledgeRequirementsContents(dialectKnowledge);
+        }
+    }
+
+    private void DisableDialectRequirementsUITemplate() => dialectKnowledgeRequirementsTemplate.gameObject.SetActive(false);
 
     public void DestroyPanel() => Destroy(gameObject);
 }
