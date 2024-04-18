9ea4a78e58d3906250ffc5fc748333c94f4abae1
Raphael <rpanduro.t@gmail.com>
2024-04-17 22:29:36 -0500

    17/04 Raphael Panduro
    ADD UI World Space Scale To Camera OrthoSize

---
+++ Assets/Scripts/UI/ScaleToCamera.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScaleToCamera : MonoBehaviour
+{
+    [SerializeField] private Mode mode;
+
+    private enum Mode { ConstantScale }
+
+    private float initialOrthoSize = 6.5f;
+
+    private void LateUpdate()
+    {
+        ScaleLogic();
+    }
+
+    private void ScaleLogic()
+    {
+        switch (mode)
+        {
+            case Mode.ConstantScale:
+                transform.localScale = CalculateScale();
+                break;
+        }
+    }
+
+    private Vector3 CalculateScale()
+    {
+        float orthographicSize = Camera.main.orthographicSize;
+        float scaleFactor = orthographicSize / initialOrthoSize;
+
+        return Vector3.one * scaleFactor;
+    }
+}
