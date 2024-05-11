d69485455b571ef41be677c3b73bfe431acca17a
Raphael <rpanduro.t@gmail.com>
2024-05-11 00:19:45 -0500

    11/05 Raphael Panduro
    ADD Inventory UI Progress

--- Assets/Scripts/Systems/DataPersistence/Models/Hybrid/ObjectsData.cs
+++ Assets/Scripts/Systems/DataPersistence/Models/Hybrid/ObjectsData.cs
@@ -5,13 +5,11 @@ using UnityEngine;
 [System.Serializable]
 public class ObjectsData
 {
-    public SerializableDictionary<int, bool> symbolSourcesCollected;
     public SerializableDictionary<int, bool> learningPlatformsUsed;
     public SerializableDictionary<int, bool> inscriptionsTranslated;
 
     public ObjectsData()
     {
-        symbolSourcesCollected = new SerializableDictionary<int, bool>(); //String -> id; bool -> isCollected
         learningPlatformsUsed = new SerializableDictionary<int, bool>(); //String -> id; bool -> isLearned
         inscriptionsTranslated = new SerializableDictionary<int, bool>(); //String -> id; bool -> isTranslated
     }
