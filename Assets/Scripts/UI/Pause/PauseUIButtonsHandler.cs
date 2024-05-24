a406c9e82dc811b74d52ef2e9ae4257bc21be5d8
Raphael <rpanduro.t@gmail.com>
2024-05-24 09:12:21 -0500

    24/05 Raphael Panduro
    Quit Game Pressing Menu Button

--- Assets/Scripts/UI/Pause/PauseUIButtonsHandler.cs
+++ Assets/Scripts/UI/Pause/PauseUIButtonsHandler.cs
@@ -8,6 +8,7 @@ public class PauseUIButtonsHandler : MonoBehaviour
 {
     [Header("UI Components")]
     [SerializeField] private Button inGameOptionsButton;
+    [SerializeField] private Button backToMenuButton;
 
     public static event EventHandler OnOpenInGameOptionsUI;
 
@@ -19,11 +20,17 @@ public class PauseUIButtonsHandler : MonoBehaviour
     private void InitializeButtonsListeners()
     {
         inGameOptionsButton.onClick.AddListener(OpenInGameOptionsUI);
+        backToMenuButton.onClick.AddListener(BackToMenu);
     }
 
     private void OpenInGameOptionsUI()
     {
         OnOpenInGameOptionsUI?.Invoke(this, EventArgs.Empty);
+    }
+
+    private void BackToMenu()
+    {
+        Application.Quit();
     }
 
 }
