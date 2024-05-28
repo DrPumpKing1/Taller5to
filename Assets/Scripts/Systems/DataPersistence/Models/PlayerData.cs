9aefd123277362adf77a1d64db5b81742bd44f5b
Raphael <rpanduro.t@gmail.com>
2024-05-28 10:01:41 -0500

    Raphael Panduro
    Shield Pieces To JSON

--- Assets/Scripts/Systems/DataPersistence/Models/PlayerData.cs
+++ Assets/Scripts/Systems/DataPersistence/Models/PlayerData.cs
@@ -8,6 +8,7 @@ public class PlayerData
     public int checkpointID;
     public SerializableDictionary<int, bool> symbolsCollected;
     public SerializableDictionary<int, bool> symbolSourcesCollected;
+    public SerializableDictionary<int, bool> shieldPiecesCollected;
     public SerializableDictionary<int, bool> projectableObjectsLearned;
     public int totalProjectionGems;
 
@@ -17,6 +18,7 @@ public class PlayerData
         symbolsCollected = new SerializableDictionary<int, bool>(); //String -> id; bool -> isCollected
         symbolSourcesCollected = new SerializableDictionary<int, bool>(); //String -> id; bool -> isCollected
         projectableObjectsLearned = new SerializableDictionary<int, bool>(); //String -> id; bool -> isLearned
+        shieldPiecesCollected = new SerializableDictionary<int, bool>();//String -> id; bool -> isCollected
         totalProjectionGems = 0;
     }
 }
