bceff0e8959cc5a1d5b716de500d97ac733417ca
Raphael <rpanduro.t@gmail.com>
2024-06-11 13:26:49 -0500

    Collider Parent

--- Assets/Scripts/Systems/Mechanics/Other/ColliderPlayerParent.cs
+++ Assets/Scripts/Systems/Mechanics/Other/ColliderPlayerParent.cs
@@ -6,11 +6,14 @@ public class ColliderPlayerParent : MonoBehaviour
 {
     private const string PLAYER_TAG = "Player";
 
+    private Transform player;
+
     private void OnCollisionEnter(Collision collision)
     {
         if (!collision.gameObject.CompareTag(PLAYER_TAG)) return;
 
         collision.transform.SetParent(transform);
+        player = collision.transform;
     }
 
     private void OnCollisionExit(Collision collision)
@@ -18,5 +21,14 @@ public class ColliderPlayerParent : MonoBehaviour
         if (!collision.gameObject.CompareTag(PLAYER_TAG)) return;
 
         collision.transform.SetParent(null);
+        player = null;
+    }
+
+    private void OnDestroy()
+    {
+        if (player)
+        {
+            player.SetParent(null);
+        }
     }
 }
