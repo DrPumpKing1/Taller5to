489cc454d4b91c8d0076363c32a264b6b7c60e41
Raphael <rpanduro.t@gmail.com>
2024-06-21 11:02:33 -0500

    DirectionalLight Controller

---
+++ Assets/Scripts/Rendering/DirectionalLightingHandler.cs
@@ -0,0 +1,76 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+using System.Linq;
+
+public class DirectionalLightingHandler : MonoBehaviour
+{
+    [Header("Components")]
+    [SerializeField] private Light directionalLight;
+    [SerializeField] private List<RoomCollider> controllingColliders;
+
+    private void OnEnable()
+    {
+        RoomManager.OnStartBlockingViewColliders += RoomVisibilityManager_OnStartBlockingViewColliders;
+        RoomManager.OnEnterBlockingViewColliders += RoomVisibilityManager_OnEnterBlockingViewColliders;
+        RoomManager.OnExitBlockingViewColliders += RoomVisibilityManager_OnExitBlockingViewColliders;
+    }
+
+    private void OnDisable()
+    {
+        RoomManager.OnStartBlockingViewColliders -= RoomVisibilityManager_OnStartBlockingViewColliders;
+        RoomManager.OnEnterBlockingViewColliders -= RoomVisibilityManager_OnEnterBlockingViewColliders;
+        RoomManager.OnExitBlockingViewColliders -= RoomVisibilityManager_OnExitBlockingViewColliders;
+    }
+
+    private void EnableDirectionalLight() => directionalLight.enabled = true;
+    private void DisableDirectionalLight() => directionalLight.enabled = false;
+
+    private void CheckStartVisibility(List<RoomCollider> currentCollidersBlockingView)
+    {
+        if (controllingColliders.Intersect(currentCollidersBlockingView).Any())
+        {
+            EnableDirectionalLight();
+        }
+        else
+        {
+            DisableDirectionalLight();
+        }
+    }
+
+    private void CheckEnterVisibility(List<RoomCollider> previousCollidersBlockingView, List<RoomCollider> enterVisibilityColliders)
+    {
+        if ((controllingColliders.Intersect(previousCollidersBlockingView).Any())) return; //A controllingCollider was already on the previous List
+
+        if (!(controllingColliders.Intersect(enterVisibilityColliders).Any())) return;
+
+        EnableDirectionalLight();
+    }
+
+    private void CheckExitVisibility(List<RoomCollider> previousCollidersBlockingView, List<RoomCollider> currentVisibilityColliders)
+    {
+        if (!(controllingColliders.Intersect(previousCollidersBlockingView).Any())) return; //There wasn't any controllingCollider on previous list (none o them was bocking view and this was already invisible)
+        //At this point, there was at least one collider blocking view
+
+        if (controllingColliders.Intersect(currentVisibilityColliders).Any()) return; //If right now there is at least one collider blocking view, return
+        //At this point, there was at least one collider blocking view, but now there is none
+
+        DisableDirectionalLight();
+    }
+
+    private void RoomVisibilityManager_OnStartBlockingViewColliders(object sender, RoomManager.OnBlockingViewCollidersStartEventArgs e)
+    {
+        CheckStartVisibility(e.currentRoomVisibilityColliders);
+    }
+
+    private void RoomVisibilityManager_OnEnterBlockingViewColliders(object sender, RoomManager.OnBlockingViewCollidersEnterEventArgs e)
+    {
+        CheckEnterVisibility(e.previousRoomVisibilityColliders, e.newRoomVisibilityColliders);
+    }
+
+    private void RoomVisibilityManager_OnExitBlockingViewColliders(object sender, RoomManager.OnBlockingViewCollidersExitEventArgs e)
+    {
+        CheckExitVisibility(e.previousRoomVisibilityColliders, e.currentRoomVisibilityColliders);
+    }
+}
