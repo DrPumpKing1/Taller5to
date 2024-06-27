7d9699c75a3f260af707b23116726ebc4d07350d
Raphael <rpanduro.t@gmail.com>
2024-06-27 11:25:03 -0500

    Boss Bar

--- Assets/Scripts/Characters/Boss/BossKaerumOvercharge.cs
+++ Assets/Scripts/Characters/Boss/BossKaerumOvercharge.cs
@@ -12,10 +12,20 @@ public class BossKaerumOvercharge : MonoBehaviour
 
     public static event EventHandler OnBossOvercharge;
     public static event EventHandler<OnBossHitEventArgs> OnBossHit;
+    public static event EventHandler<OnCurrentHitsInPhaseChangedEventArgs> OnCurrentHitsInPhaseChanged;
+    public static event EventHandler OnCurrentHitsInPhaseReset;
+
+    public int CurrentHitsInPhase => currentHitsInPhase;
 
     public class OnBossHitEventArgs : EventArgs
     {
         public bool isInvulnerable;
+    }
+
+    public class OnCurrentHitsInPhaseChangedEventArgs: EventArgs
+    {
+        public int currentHitsInPhase;
+        public int hitsPerPhase;
     }
 
     private void Start()
@@ -34,7 +44,12 @@ public class BossKaerumOvercharge : MonoBehaviour
 
         OnBossHit?.Invoke(this, new OnBossHitEventArgs { isInvulnerable = BossPhaseHandler.Instance.isInvulnerable });
 
-        if (!BossPhaseHandler.Instance.isInvulnerable) currentHitsInPhase++;
+        if (!BossPhaseHandler.Instance.isInvulnerable)
+        {
+            currentHitsInPhase++;
+            OnCurrentHitsInPhaseChanged?.Invoke(this, new OnCurrentHitsInPhaseChangedEventArgs { currentHitsInPhase = currentHitsInPhase, hitsPerPhase = hitsPerPhase });
+        }
+
         CheckOvercharge();
     }
 
@@ -47,7 +62,11 @@ public class BossKaerumOvercharge : MonoBehaviour
         }
     }
 
-    private void ResetCurrentHitsInPhase() => currentHitsInPhase = 0;
+    private void ResetCurrentHitsInPhase()
+    {
+        currentHitsInPhase = 0;
+        OnCurrentHitsInPhaseReset?.Invoke(this, EventArgs.Empty);
+    }
 
     private void OnCollisionEnter(Collision collision)
     {
--- Assets/Scripts/StaticClasses/GeneralUIMethods.cs
+++ Assets/Scripts/StaticClasses/GeneralUIMethods.cs
@@ -9,4 +9,5 @@ public static class GeneralUIMethods
     public static void SetCanvasGroupAlpha(CanvasGroup canvasGroup, float alpha) => canvasGroup.alpha = alpha;
 
     public static void SetImageFillRatio(Image image, float fillRatio) => image.fillAmount = fillRatio;
+    public static void SetImageColor(Image image, Color color) => image.color = color;
 }
