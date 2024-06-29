6b80625be7eb3df6d245d38dc777fbf8bbbdb5bc
Raphael <rpanduro.t@gmail.com>
2024-06-29 09:59:43 -0500

    Fade After Player Lose

---
+++ Assets/Scripts/Game/LoseManager.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LoseManager : MonoBehaviour
+{
+    [Header("Settings")]
+    [SerializeField,Range(1f,10f)] private float timeToReloadAfterLose;
+
+    private void OnEnable()
+    {
+        BossStateHandler.OnPlayerDefeated += BossStateHandler_OnPlayerDefeated;
+    }
+
+    private void OnDisable()
+    {
+        BossStateHandler.OnPlayerDefeated -= BossStateHandler_OnPlayerDefeated;
+    }
+
+    private void LoseReload()
+    {
+        StopAllCoroutines();
+        StartCoroutine(LoseReloadCoroutine());
+    }
+
+    private IEnumerator LoseReloadCoroutine()
+    {
+        yield return new WaitForSeconds(timeToReloadAfterLose);
+        ScenesManager.Instance.FadeReloadCurrentScene();
+    }
+
+    private void BossStateHandler_OnPlayerDefeated(object sender, System.EventArgs e)
+    {
+        LoseReload();
+    }
+}
