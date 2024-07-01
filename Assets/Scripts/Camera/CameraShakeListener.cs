0a1515074cbb3e2c16f6f8d46cc3ae78960bccbe
Raphael <rpanduro.t@gmail.com>
2024-06-30 20:38:28 -0500

    BossDoor & BossShakes

--- Assets/Scripts/Camera/CameraShakeListener.cs
+++ Assets/Scripts/Camera/CameraShakeListener.cs
@@ -11,18 +11,58 @@ public class CameraShakeListener : MonoBehaviour
     [SerializeField,Range(0f,10f)] private float virtueDoorShakeFadeInTime;
     [SerializeField,Range(0f,10f)] private float virtueDoorShakeFadeOutTime;
 
+    [Header("Boss Door Settings")]
+    [SerializeField, Range(0, 1f)] private float bossDoorShakeAmplitude;
+    [SerializeField, Range(0, 5f)] private float bossDoorShakeFrequency;
+    [SerializeField, Range(0.5f, 10f)] private float bossDoorShakeTime;
+    [SerializeField, Range(0f, 10f)] private float bossDoorShakeFadeInTime;
+    [SerializeField, Range(0f, 10f)] private float bossDoorShakeFadeOutTime;
+
+    [Header("Boss Active Settings")]
+    [SerializeField, Range(0, 1f)] private float bossActiveShakeAmplitude;
+    [SerializeField, Range(0, 5f)] private float bossActiveShakeFrequency;
+    [SerializeField, Range(0.5f, 10f)] private float bossActiveShakeTime;
+    [SerializeField, Range(0f, 10f)] private float bossActiveShakeFadeInTime;
+    [SerializeField, Range(0f, 10f)] private float bossActiveShakeFadeOutTime;
+
+    [Header("Boss PhaseChange Settings")]
+    [SerializeField, Range(0, 1f)] private float bossPhaseChangeShakeAmplitude;
+    [SerializeField, Range(0, 5f)] private float bossPhaseChangeShakeFrequency;
+    [SerializeField, Range(0.5f, 10f)] private float bossPhaseChangeShakeTime;
+    [SerializeField, Range(0f, 10f)] private float bossPhaseChangeShakeFadeInTime;
+    [SerializeField, Range(0f, 10f)] private float bossPhaseChangeShakeFadeOutTime;
+
+    [Header("Boss Defeated Settings")]
+    [SerializeField, Range(0, 1f)] private float bossDefeatedShakeAmplitude;
+    [SerializeField, Range(0, 5f)] private float bossDefeatedShakeFrequency;
+    [SerializeField, Range(0.5f, 10f)] private float bossDefeatedShakeTime;
+    [SerializeField, Range(0f, 10f)] private float bossDefeatedShakeFadeInTime;
+    [SerializeField, Range(0f, 10f)] private float bossDefeatedShakeFadeOutTime;
+
     private void OnEnable()
     {
         ShieldDoor.OnShieldDoorOpen += ShieldDoor_OnShieldDoorOpen;
+        //BossDoor.OnBossDoorOpen += BossDoor_OnBossDoorOpen;
+        BossStateHandler.OnBossActiveStart += BossStateHandler_OnBossActiveStart;
+        BossStateHandler.OnBossPhaseChangeStart += BossStateHandler_OnBossPhaseChangeStart;
+        BossStateHandler.OnBossDefeated += BossStateHandler_OnBossDefeated;
     }
 
     private void OnDisable()
     {
         ShieldDoor.OnShieldDoorOpen -= ShieldDoor_OnShieldDoorOpen;
+        //BossDoor.OnBossDoorOpen -= BossDoor_OnBossDoorOpen;
+        BossStateHandler.OnBossActiveStart -= BossStateHandler_OnBossActiveStart;
+        BossStateHandler.OnBossPhaseChangeStart -= BossStateHandler_OnBossPhaseChangeStart;
+        BossStateHandler.OnBossDefeated -= BossStateHandler_OnBossDefeated;
     }
 
-    private void ShieldDoor_OnShieldDoorOpen(object sender, ShieldDoor.OnShieldDoorOpenEventArgs e)
-    {
-        CameraShakeHandler.Instance.ShakeCamera(virtueDoorShakeAmplitude, virtueDoorShakeFrequency, virtueDoorShakeTime, virtueDoorShakeFadeInTime, virtueDoorShakeFadeOutTime);
-    }
+
+
+
+    private void ShieldDoor_OnShieldDoorOpen(object sender, ShieldDoor.OnShieldDoorOpenEventArgs e) => CameraShakeHandler.Instance.ShakeCamera(virtueDoorShakeAmplitude, virtueDoorShakeFrequency, virtueDoorShakeTime, virtueDoorShakeFadeInTime, virtueDoorShakeFadeOutTime);
+    private void BossDoor_OnBossDoorOpen(object sender, System.EventArgs e) => CameraShakeHandler.Instance.ShakeCamera(bossDoorShakeAmplitude, bossDoorShakeFrequency, bossDoorShakeTime, bossDoorShakeFadeInTime, bossDoorShakeFadeOutTime);
+    private void BossStateHandler_OnBossActiveStart(object sender, System.EventArgs e) => CameraShakeHandler.Instance.ShakeCamera(bossActiveShakeAmplitude, bossActiveShakeFrequency, bossActiveShakeTime, bossActiveShakeFadeInTime, bossActiveShakeFadeOutTime);
+    private void BossStateHandler_OnBossPhaseChangeStart(object sender, BossStateHandler.OnPhaseChangeEventArgs e) => CameraShakeHandler.Instance.ShakeCamera(bossPhaseChangeShakeAmplitude, bossPhaseChangeShakeFrequency, bossPhaseChangeShakeTime, bossPhaseChangeShakeFadeInTime, bossPhaseChangeShakeFadeOutTime);
+    private void BossStateHandler_OnBossDefeated(object sender, System.EventArgs e) => CameraShakeHandler.Instance.ShakeCamera(bossDefeatedShakeAmplitude, bossDefeatedShakeFrequency, bossDefeatedShakeTime, bossDefeatedShakeFadeInTime, bossDefeatedShakeFadeOutTime);
 }
--- Assets/Scripts/Systems/Mechanics/Other/BossDoor.cs
+++ Assets/Scripts/Systems/Mechanics/Other/BossDoor.cs
@@ -1,18 +1,60 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System;
 
 public class BossDoor : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    [Header("Device Settings")]
+    [SerializeField] private Transform gateTransform;
+    [SerializeField] private Transform restPosition;
+    [SerializeField] private Transform powerPosition;
+    [SerializeField] private float moveSpeed;
+    [SerializeField] private AnimationCurve moveCurve;
+
+    public static event EventHandler OnBossDoorOpen;
+
+    private Coroutine doorMovement;
+
+    private void OnEnable()
     {
-        
+        BossStateHandler.OnBossDefeated += BossStateHandler_OnBossDefeated;
     }
 
-    // Update is called once per frame
-    void Update()
+    private void OnDisable()
     {
-        
+        BossStateHandler.OnBossDefeated -= BossStateHandler_OnBossDefeated;
+    }
+
+    private void TriggerMovement(bool state)
+    {
+        if (doorMovement != null) StopCoroutine(doorMovement);
+        doorMovement = StartCoroutine(MoveToPosition(state ? powerPosition.position : restPosition.position));
+    }
+
+    private IEnumerator MoveToPosition(Vector3 position)
+    {
+        Vector3 startPosition = gateTransform.position;
+        Vector3 endPosition = position;
+        float distance = Vector3.Distance(startPosition, endPosition);
+        float time = distance / moveSpeed;
+        float t = 0f;
+
+        while (t < time)
+        {
+            t += Time.deltaTime;
+
+            gateTransform.position = Vector3.Lerp(startPosition, endPosition, moveCurve.Evaluate(t / time));
+
+            yield return null;
+        }
+
+        gateTransform.position = endPosition;
+    }
+
+    private void BossStateHandler_OnBossDefeated(object sender, EventArgs e)
+    {
+        TriggerMovement(true);
+        OnBossDoorOpen?.Invoke(this, EventArgs.Empty);    
     }
 }
