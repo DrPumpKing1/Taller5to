3032072f5d1fc946a8259eb0460a829977014324
Raphael <rpanduro.t@gmail.com>
2024-06-30 20:17:02 -0500

    Ancient Relic Door 2

--- Assets/Scripts/Systems/Mechanics/Other/AncientRelicDoor.cs
+++ Assets/Scripts/Systems/Mechanics/Other/AncientRelicDoor.cs
@@ -9,5 +9,69 @@ public class AncientRelicDoor : MonoBehaviour
     [SerializeField] private Transform transformToDisable;
 
     [Header("Settings")]
+    [SerializeField] private float timeDisabledAfterDepowered;
     [SerializeField] private List<Electrode> controllingElectrodes;
+
+    private bool previouslyPowered;
+    public float timer;
+
+    public const float ACTIVATION_THRESHOLD = 20f;
+
+    private void Start()
+    {
+        InitializeVariables();
+    }
+
+    private void Update()
+    {
+        HandlePowered();
+    }
+
+    private void InitializeVariables()
+    {
+        previouslyPowered = false;
+        timer = 0f;
+    }
+
+    private void HandlePowered()
+    {
+        if(AllControllingElectrodesDePowered())
+        {
+            ResetTimer();
+
+            if (previouslyPowered)
+            {
+                DisableDoor();
+                previouslyPowered = false;
+            }
+
+            return;
+        }
+        
+        if(!AllControllingElectrodesDePowered())
+        {
+            timer += Time.deltaTime;
+
+            if (timer >= timeDisabledAfterDepowered && !previouslyPowered)
+            {
+                EnableDoor();
+                previouslyPowered = true;
+            }
+        }
+
+    }
+
+    private bool AllControllingElectrodesDePowered()
+    {
+        foreach(Electrode electrode in controllingElectrodes)
+        {
+            if (electrode.Power >= ACTIVATION_THRESHOLD) return false;
+        }
+
+        return true;
+    }
+
+    private void DisableDoor() => transformToDisable.gameObject.SetActive(false);
+    private void EnableDoor() => transformToDisable.gameObject.SetActive(true);
+    private void ResetTimer() => timer = 0f;
 }
