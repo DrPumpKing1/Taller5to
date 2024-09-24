b14eac2be7e19782f3bbbf1e38d3a376a5e82f1b
Raphael <115659111+VCliqu3@users.noreply.github.com>
2024-09-23 20:50:51 -0500

    EventsFixed

--- Assets/Scripts/UI/Gameplay/ShieldsPopUp/ShieldsPopUpUI.cs
+++ Assets/Scripts/UI/Gameplay/ShieldsPopUp/ShieldsPopUpUI.cs
@@ -14,10 +14,11 @@ public class ShieldsPopUpUI : MonoBehaviour
     private const string SHOW_TRIGGER = "Show";
     private const string HIDE_TRIGGER = "Hide";
 
-    private const float SHOW_TIME = 0.5f;
-    private const float HIDE_TIME = 0.5f;
+    private const float SHOW_TIME = 1f;
+    private const float HIDE_TIME = 1f;
 
-    public event EventHandler OnShieldPopUp;
+    public static event EventHandler<OnShieldPopUpEventArgs> OnShieldPopUpShow;
+    public static event EventHandler<OnShieldPopUpEventArgs> OnShieldPopUpHide;
 
     public class OnShieldPopUpEventArgs: EventArgs
     {
@@ -45,18 +46,21 @@ public class ShieldsPopUpUI : MonoBehaviour
         shieldsPopUpUIAnimator.SetTrigger(HIDE_TRIGGER);
     }
 
-    private IEnumerator PopUpShieldCoroutine()
+    private IEnumerator PopUpShieldCoroutine(ShieldPieceSO shieldPieceSO)
     {
+        OnShieldPopUpShow?.Invoke(this, new OnShieldPopUpEventArgs { shieldPieceSO = shieldPieceSO });
+
         ShowPopUp();
         yield return new WaitForSeconds(SHOW_TIME);
         yield return new WaitForSeconds(timeShowingPopUp);
         HidePopUp();
         yield return new WaitForSeconds(HIDE_TIME);
+
+        OnShieldPopUpHide?.Invoke(this, new OnShieldPopUpEventArgs { shieldPieceSO = shieldPieceSO });
     }
 
     private void ShieldPiecesManager_OnShieldPieceCollected(object sender, ShieldPiecesManager.OnShieldPieceCollectedEventArgs e)
     {
-        OnShieldPopUp?.Invoke(this, new OnShieldPopUpEventArgs{shieldPieceSO = e.shieldPieceSO});
-        StartCoroutine(PopUpShieldCoroutine());
+        StartCoroutine(PopUpShieldCoroutine(e.shieldPieceSO));
     }
 }
