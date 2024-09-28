0175b54f6e08685efdb0b9c409385be6ded5ddb4
Raphael <115659111+VCliqu3@users.noreply.github.com>
2024-09-27 22:39:51 -0500

    PreBuild 27-09

--- Assets/Scripts/Systems/Instructions/Classes/LogInstruction.cs
+++ Assets/Scripts/Systems/Instructions/Classes/LogInstruction.cs
@@ -24,5 +24,4 @@ public class LogInstruction : Instruction
         if (e.gameplayAction.log != logToShow) return;
         CheckShouldShow();
     }
-
 }
--- Assets/Scripts/Systems/Instructions/Classes/Tutorial/GoBackToDoahInstruction.cs
+++ Assets/Scripts/Systems/Instructions/Classes/Tutorial/GoBackToDoahInstruction.cs
@@ -2,24 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class GoBackToDoahInstruction : Instruction
+public class GoBackToDoahInstruction : LogInstruction
 {
-    protected override void OnEnable()
-    {
-        base.OnEnable();
-        InventoryOpeningManager.OnInventoryClose += InventoryOpeningManager_OnInventoryClose;
-    }
-
-    protected override void OnDisable()
-    {
-        base.OnDisable();
-        InventoryOpeningManager.OnInventoryClose -= InventoryOpeningManager_OnInventoryClose;
-    }
-
-    private void InventoryOpeningManager_OnInventoryClose(object sender, System.EventArgs e)
-    {
-        CheckShouldShow();
-    }
-
     protected override bool CheckCondition() => ProjectableObjectsLearningManager.Instance.ProjectableObjectsLearned.Count <= 0;
 }
