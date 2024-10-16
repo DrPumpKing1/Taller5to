70157dfccb7d519e83f0b7329057e17a4ae2a94b
Raphael <115659111+VCliqu3@users.noreply.github.com>
2024-10-16 10:47:41 -0500

    Boss Platform Dematerialization VFX

--- Assets/Scripts/Characters/Boss/Visuals/BeamPlatformDematerializationVFXHandler.cs
+++ Assets/Scripts/Characters/Boss/Visuals/BeamPlatformDematerializationVFXHandler.cs
@@ -1,9 +1,20 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.VFX;
 
-public class BeamPlatformDematerializationVisual : MonoBehaviour
+public class BeamPlatformDematerializationVFXHandler : MonoBehaviour
 {
+    [Header("Components")]
+    [SerializeField] private VisualEffect platformDematerializationVFX;
+
+    [Header("Settings")]
+    [SerializeField, Range(10f, 20f)] private float lightningHeight;
+    [SerializeField, Range(0.1f, 2f)] private float lightningLifetime;
+
+    private const string LIGHTNING_HEIGHT_PROPERTY = "LightningHeight";
+    private const string LIGHTNING_LIFETIME_PROPERTY = "LightningLifeTime";
     private void OnEnable()
     {
         BossBeam.OnBeamObjectDematerialization += BossBeam_OnBeamObjectDematerialization;
@@ -14,10 +25,42 @@ public class BeamPlatformDematerializationVisual : MonoBehaviour
         BossBeam.OnBeamObjectDematerialization -= BossBeam_OnBeamObjectDematerialization;
     }
 
+
+    private void Start()
+    {
+        InitializeVFX();
+    }
+
+    private void InitializeVFX()
+    {
+        InitializeFloatProperty(LIGHTNING_HEIGHT_PROPERTY,lightningHeight);
+        InitializeFloatProperty(LIGHTNING_LIFETIME_PROPERTY, lightningLifetime);
+        platformDematerializationVFX.Stop();
+    }
+
+    private void InitializeFloatProperty(string propertyName, float propertyValue)
+    {
+        if (!platformDematerializationVFX.HasFloat(propertyName)) return;
+
+        platformDematerializationVFX.SetFloat(propertyName, propertyValue);
+    }
+
+    private void RelocateLightningOrigin(Vector3 position)
+    {
+        Vector3 newOrigin = new Vector3(position.x, position.y + lightningHeight, position.z);
+        platformDematerializationVFX.transform.position = newOrigin;
+    }
+
+    private void DematerializePlatformVFX(Vector3 position)
+    {
+        RelocateLightningOrigin(position);
+        platformDematerializationVFX.Play();
+    }
+
     #region BossBeam Subscriptions
     private void BossBeam_OnBeamObjectDematerialization(object sender, BossBeam.OnBeamObjectDematerializationEventArgs e)
     {
-        throw new System.NotImplementedException();
+        DematerializePlatformVFX(e.stunableProjectionPlatformProjection.transform.position);
     }
     #endregion
 }
