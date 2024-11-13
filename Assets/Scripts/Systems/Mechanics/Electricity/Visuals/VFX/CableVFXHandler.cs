aee204017ff71ab354788e6eebcafb420d15409b
Raphael <115659111+VCliqu3@users.noreply.github.com>
2024-11-13 12:28:22 -0500

    Test Cable VFX On

--- Assets/Scripts/Systems/Mechanics/Electricity/Visuals/VFX/CableVFXHandler.cs
+++ Assets/Scripts/Systems/Mechanics/Electricity/Visuals/VFX/CableVFXHandler.cs
@@ -11,7 +11,25 @@ public class CableVFXHandler : MonoBehaviour
 
     [Header("Settings")]
     [SerializeField] private bool enableVFX = true;
+
+    [Header("Cable On Settings")]
+    [SerializeField, ColorUsage(true, true)] private Color onColor;
+    [SerializeField, Range (0f,300f)] private float onParticleRatio;
+    [SerializeField, Range(0f,0.15f)] private float onParticleScale;
+    [SerializeField, Range(0f,0.1f)] private float onDispersion;
+
+    [Header("Cable Off Settings")]
+    [SerializeField, ColorUsage(true, true)] private Color offColor;
+    [SerializeField, Range(0f, 300f)] private float offParticleRatio;
+    [SerializeField, Range(0f, 0.15f)] private float offParticleScale;
+    [SerializeField, Range(0f, 0.1f)] private float offDispersion;
+
     private bool IsPowered => electrode.Power >= Electrode.ACTIVATION_THRESHOLD;
+
+    private const string COLOR_PROPERTY = "ParticleColor";
+    private const string PARTICLE_RATIO_PROPERTY = "ParticleRatio";
+    private const string PARTICLE_SCALE_PROPERTY = "ParticleScale";
+    private const string DISPERSION_PROPERTY = "Dispersion";
 
     private const float NOT_POWERED_TIME_THRESHOLD = 0.5f;
     private float notPoweredTimer;
@@ -19,7 +37,8 @@ public class CableVFXHandler : MonoBehaviour
 
     private void Awake()
     {
-        TurnVFX(false);
+        TurnVFX(false); //TurnVFX(true)
+        //SetVFXOn(false);
     }
 
     private void LateUpdate()
@@ -35,6 +54,7 @@ public class CableVFXHandler : MonoBehaviour
 
             if (notPoweredTimer >= NOT_POWERED_TIME_THRESHOLD && previousPowered)
             {
+                //SetVFXOn(false);
                 TurnVFX(false);
                 previousPowered = false;
             }
@@ -43,6 +63,7 @@ public class CableVFXHandler : MonoBehaviour
         {
             if (!previousPowered)
             {
+                //SetVFXOn(true);
                 TurnVFX(true);
             }
 
@@ -60,6 +81,68 @@ public class CableVFXHandler : MonoBehaviour
         else
         {
             cableVFX.Stop();
+        }
+    }
+
+    private void SetVFXColor(Color color)
+    {
+        if (!cableVFX.HasVector4(COLOR_PROPERTY))
+        {
+            Debug.Log("PropertyNotFound");
+            return;
+        }
+
+        cableVFX.SetVector4(COLOR_PROPERTY, color);
+    }
+
+    private void SetVFXParticleRatio(float particleRatio)
+    {
+        if (!cableVFX.HasFloat(PARTICLE_RATIO_PROPERTY))
+        {
+            Debug.Log("PropertyNotFound");
+            return;
+        }
+
+        cableVFX.SetFloat(PARTICLE_RATIO_PROPERTY, particleRatio);
+    }
+
+    private void SetVFXParticleScale(float particleScale)
+    {
+        if (!cableVFX.HasFloat(PARTICLE_SCALE_PROPERTY))
+        {
+            Debug.Log("PropertyNotFound");
+            return;
+        }
+
+        cableVFX.SetFloat(PARTICLE_SCALE_PROPERTY, particleScale);
+    }
+
+    private void SetVFXDispersion(float dispersion)
+    {
+        if (!cableVFX.HasFloat(DISPERSION_PROPERTY))
+        {
+            Debug.Log("PropertyNotFound");
+            return;
+        }
+
+        cableVFX.SetFloat(DISPERSION_PROPERTY, dispersion);
+    }
+
+    private void SetVFXOn(bool cableOn)
+    {
+        if (cableOn)
+        {
+            SetVFXColor(onColor);
+            SetVFXParticleRatio(onParticleRatio);
+            SetVFXParticleScale(onParticleScale);
+            SetVFXDispersion(onDispersion);
+        }
+        else
+        {
+            SetVFXColor(offColor);
+            SetVFXParticleRatio(offParticleRatio);
+            SetVFXParticleScale(offParticleScale);
+            SetVFXDispersion(offDispersion);
         }
     }
 }
